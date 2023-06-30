73975e12ae5b885b8d80c7615e14124b18402264
xinchuant <1902235217@qq.com>
2023-06-30 21:00:51 +0800

    .

--- a.c
+++ a.c
@@ -1,61 +1,123 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#define N 100001
+void swap(int *a, int *b);
 
-void swap(int *x,int *y);
-void quick_sort_recursive(int arr[], int start, int end);
-void quick_sort(int arr[], int len);
+int partition(int array[], int low, int high);
+
+void quickSort(int array[], int low, int high);
+
+void merge(int arr[], int low, int middle, int high);
+
+void mergeSort(int arr[], int low, int high);
 
 int main()
 {
-    int a[10000];
-    int count = 0;
-    int temp;
-    scanf("%d",&temp);
-    while(temp!=10)
+    int data[N];
+    for (int i = 0; i < N;i++)
     {
-        a[count++] = temp;
-        printf("Yes\n");
-        scanf("%d",&temp);
+        data[i] = rand();
     }
-    quick_sort(a,count);
-    int i = 0;
-    for(;i<count-1;i++)
+    // quickSort(data, 0, N - 1);
+    mergeSort(data, 0, N - 1);
+    for (int i = 0; i < N - 1; i++)
     {
-        printf("%d ",a[i]);
+        printf("%d:%d\n",i, data[i]);
     }
-    printf("%d\n",a[i]);
-}
-//test
+    printf("%d\n",data[N-1]);
+    return 0;
 
-void swap(int *x, int *y)
-{
-    int t = *x;
-    *x = *y;
-    *y = t;
+    clock();
 }
 
-void quick_sort_recursive(int arr[], int start, int end)
+void swap(int *a, int *b)
 {
-    if (start >= end)
-        return;
-    int mid = arr[end];
-    int left = start, right = end - 1;
-    while (left < right) {
-        while (arr[left] < mid && left < right)
-            left++;
-        while (arr[right] >= mid && left < right)
-            right--;
-        swap(&arr[left], &arr[right]);
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int array[], int low, int high)
+{
+    int pivot = array[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (array[j] <= pivot)
+        {
+            i++;
+            swap(&array[i], &array[j]);
+        }
     }
-    if (arr[left] >= arr[end])
-        swap(&arr[left], &arr[end]);
-    else
-        left++;
-    if (left)
-        quick_sort_recursive(arr, start, left - 1);
-    quick_sort_recursive(arr, left + 1, end);
+    swap(&array[i + 1], &array[high]);
+    return (i + 1);
 }
 
-void quick_sort(int arr[], int len) 
+void quickSort(int array[], int low, int high)
 {
-    quick_sort_recursive(arr, 0, len - 1);
+    if (low < high)
+    {
+        int pi = partition(array, low, high);
+        quickSort(array, low, pi - 1);
+        quickSort(array, pi + 1, high);
+    }
+}
+
+void merge(int arr[], int low, int middle, int high)
+{
+    int i, j, k;
+    int left = middle - low + 1;
+    int right = high - middle;
+
+    int L[left], R[right];
+
+    for (i = 0; i < left; i++)
+        L[i] = arr[low + i];
+    for (j = 0; j < right; j++)
+        R[j] = arr[middle + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = low;
+    while (i < left && j < right)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < left)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < right)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int middle = low + (high - low) / 2;
+        mergeSort(arr, low, middle);
+        mergeSort(arr, middle + 1, high);
+        merge(arr, low, middle, high);
+    }
 }
