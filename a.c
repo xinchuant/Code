de32114f51f1def61cc916e01a74f853395be624
xinchuan <1902235217@qq.com>
2023-09-19 17:39:17 +0800

    .

--- a.c
+++ a.c
@@ -4,15 +4,9 @@
 #include <string.h>
 
 unsigned short unsigned_decimal_binary(char code[], int start, int end);
+short signed_decimal_binary(char code[], int start, int end);
 
-unsigned short R0 = 0x7777;
-unsigned short R1 = 0x7777;
-unsigned short R2 = 0x7777;
-unsigned short R3 = 0x7777;
-unsigned short R4 = 0x7777;
-unsigned short R5 = 0x7777;
-unsigned short R6 = 0x7777;
-unsigned short R7 = 0x7777;
+unsigned short R[8] = {0x7777,0x7777,0x7777,0x7777,0x7777,0x7777,0x7777, 0x7777};
 
 int main ()
 {
@@ -21,18 +15,19 @@ int main ()
     while (1)
     {
         scanf("%s", code[line]);
-        if(line == 0)
-        {
-            unsigned short start_address = unsigned_decimal_binary(code[line], 0, 15);
-            printf("x%04hX\n", start_address);
-        }
         if(code[line][0] == EOF)
+            break;
+        if(code[line][0] == '&')
             break;
         line++;
     }
     unsigned short start_address = unsigned_decimal_binary(code[line],0,15);
-
+    for (int i = 0; i < 8;i++)
+    {
+        printf("R%d = x%04hX\n", i, R[i]);
+    }
 }
+
 unsigned short unsigned_decimal_binary(char code[], int start, int end)
 {
     unsigned short a = 1;
@@ -45,5 +40,53 @@ unsigned short unsigned_decimal_binary(char code[], int start, int end)
         a *= 2;
         point--;
     }
+    return ret;
+}
+
+short signed_decimal_binary(char code[], int start, int end)
+{
+    short flag = 1;
+    short ret = 0;
+    // 复制字符串,防止更改原二进制码
+    char code_copy[16];
+    for (int i = 0; i < 16; i++)
+    {
+        code_copy[i] = code[i];
+    }
+    if (code_copy[start] == '1')
+    {
+        flag = -1;
+        // 取反加一
+        int point = start;
+        while(point <= end)
+        {
+            if(code_copy[point] == '1')
+                code_copy[point] = '0';
+            else
+                code_copy[point] = '1';
+            point++;
+        }
+        int carry = 1;//将加的一视为第一位的carry
+        point = end;
+        while (point >= start)
+        {
+            if(carry)
+            {
+                if(code_copy[point] == '1')
+                {
+                    code_copy[point] = 0;
+                    carry = 1;
+                }
+                else
+                {
+                    code_copy[point] = 1;
+                    carry = 0;
+                }
+            }
+            else
+                break;//如果carry为0则后续carry均为0，直接结束即可
+        }
+    }
+    ret = flag * unsigned_decimal_binary(code_copy, start, end);
     return ret;
 }
