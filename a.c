09a25282912709e424302073d341392588094949
xinchuan <1902235217@qq.com>
2023-09-19 16:35:11 +0800

    .

--- a.c
+++ a.c
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <string.h>
 
+unsigned short unsigned_decimal_binary(char code[], int start, int end);
+
 unsigned short R0 = 0x7777;
 unsigned short R1 = 0x7777;
 unsigned short R2 = 0x7777;
@@ -19,11 +21,29 @@ int main ()
     while (1)
     {
         scanf("%s", code[line]);
-        printf("%s", code[line]);
+        if(line == 0)
+        {
+            unsigned short start_address = unsigned_decimal_binary(code[line], 0, 15);
+            printf("x%04hX\n", start_address);
+        }
         if(code[line][0] == EOF)
             break;
         line++;
     }
-    unsigned short start_address;
+    unsigned short start_address = unsigned_decimal_binary(code[line],0,15);
 
+}
+unsigned short unsigned_decimal_binary(char code[], int start, int end)
+{
+    unsigned short a = 1;
+    unsigned short ret = 0;
+    int point = end;
+    while (point >= start)
+    {
+        if(code[point] == '1')
+            ret += a;
+        a *= 2;
+        point++;
+    }
+    return ret;
 }
