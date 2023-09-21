6275c2ef3163ec386877de1849528e985b517225
xinchuan <1902235217@qq.com>
2023-09-21 16:15:52 +0800

    .

--- Lab6.c
+++ Lab6.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-#include <string.h>
 
 unsigned short unsigned_binary_to_decimal(char c[], int start, int end);
 short signed_binary_to_decimal(char c[], int start, int end);
