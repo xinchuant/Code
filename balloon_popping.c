d23a90f4dfdf8d6ee4132ea700bf40332afb9126
xinchuan <1902235217@qq.com>
2023-09-28 11:58:02 +0800

    .

--- balloon_popping.c
+++ balloon_popping.c
@@ -2,6 +2,13 @@
 
 int main()
 {
+    int n, height;
+    scanf("%d %d", &n, &height);
+    int ballon_positon[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &ballon_positon[i]);
+    int positon_to_jump = ballon_positon[0];
+    int ballon_popped = 1;
     
     return 0;
 }
