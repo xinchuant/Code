fa9b771726278dd70a01c639608f41554011e9c6
xinchuan <1902235217@qq.com>
2024-05-30 18:39:21 +0800

    。

---
+++ a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    for (int i = 0; i < 10;i++)
+    {
+        v.push_back(i);
+    }
+    
+}
